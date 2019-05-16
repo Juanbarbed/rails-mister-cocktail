6217260e87e5ab27dc03da29b1e8b79612b85f7d
JuanBarbed <juanbarbed@gmail.com>
2019-05-16 23:51:28 +0200

    cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Lemonade")
+Cocktail.create(name: "Lamanita")
+Cocktail.create(name: "Malacatoncio")
+Cocktail.create(name: "Barrabas")
+
+Dose.create(name: "una de cal", ingredient_id: 2, cocktail_id: 1)
+Dose.create(name: "cardamomo", ingredient_id: 3, cocktail_id: 1)
+Dose.create(name: "mango", ingredient_id: 4, cocktail_id: 2)
+Dose.create(name: "lavanda", ingredient_id: 1, cocktail_id: 2)
+Dose.create(name: "miel", ingredient_id: 2, cocktail_id: 2)
+Dose.create(name: "marmota", ingredient_id: 3, cocktail_id: 2)
